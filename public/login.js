47708dcd5b92097d116dd974a508c9b4d62d7bb6
Aditya Raval <adi7017@gmail.com>
2018-02-13 16:03:30 +0530

    Initial commit with SERVERURL

--- public/login.js
+++ public/login.js
@@ -1,11 +1,13 @@
 $("#loginbtn").on('click',function(){
+    //var LOCALURL = "http://localhost:3000/api/"
+    var SERVERURL = "hhttps://authapp-angularjs.herokuapp.com/api/";
     var username =$("#uname").val();
     var password =$("#pwd").val();
     if(username==="" || password===""){
         swal("Error!", "Please fill proper credentials!", "error");
     }else{
         $('#loginbtn').text('Logging In..');
-        $.post('http://localhost:3000/api/login', {
+        $.post(SERVERURL+'login', {
         email: username,
         password: password
         }, function(auth) {
